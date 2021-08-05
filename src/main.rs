a0bdab11a7452b2ae46f080ec4d171a627f5f327
sokorototo <nyachiengatoto@gmail.com>
2021-08-05 04:06:44 +0300

    Tested ArchiveConfig

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 #![allow(unused_variables)]
 use std::{fs::File, io::{BufReader, Read}, str};
 
-use crate::loader::ArchiveConfig;
-mod loader;
+pub(crate) mod loader;
 
 fn main(){
 	let file = File::open("me.vach").unwrap();
@@ -17,16 +16,22 @@ fn main(){
 		 false => println!("Invalid archive file")
 	};
 	
-	// Just some tests
+	// TODO: Put this in libs.rs under #[cfg(tests)]
 	{
 		let mut archive_opt = loader::ArchiveConfig::default();
 		println!("{}", &archive_opt);
 
-		assert_eq!(archive_opt.flags, 0u16);
-		archive_opt.set_flags(16u16);
+		assert_eq!(archive_opt.flags, 0);
+		archive_opt.set_flags(16);
 		
-		assert_eq!(archive_opt.flags, 16u16);
+		assert_eq!(archive_opt.flags, 16);
 		assert_eq!(archive_opt.magic, *b"VfACH");
 		assert_eq!(archive_opt.minimum_version, 0);
+
+		archive_opt.toggle_flag(0b10010101, true);
+		assert_eq!(archive_opt.flags, 0b10010101);
+		archive_opt.toggle_flag(0b10010101, false);
+		assert_eq!(archive_opt.flags, 0);
+		println!("ArchiveConfig test passed");
 	}
 }
