c57a42f6338629d4f3079bfb077306f4b3b97a3e
sokorototo <nyachiengatoto@gmail.com>
2021-08-19 23:10:21 +0300

    Removed mutability and renamed storage to handle

--- src/loader/archive.rs
+++ src/loader/archive.rs
@@ -4,13 +4,13 @@ use crate::global::{
     header::{Header, HeaderConfig},
     registry::{Registry, RegistryEntry},
 };
-use std::{fmt, io::{BufReader, BufWriter, Cursor, Read, Seek, SeekFrom, Write}, str, sync::Arc};
+use std::{fmt, io::{Cursor, Read, Seek, SeekFrom}, str, sync::Arc};
 
 #[derive(Debug)]
-pub struct Archive<T> {
+pub struct Archive<T> { 
     header: Header,
     registry: Registry,
-    storage: T,
+    handle: T,
 }
 
 impl Archive<Cursor<Vec<u8>>> {
@@ -18,73 +18,62 @@ impl Archive<Cursor<Vec<u8>>> {
         Archive {
             header: Header::empty(),
             registry: Registry::empty(),
-            storage: Cursor::new(Vec::new()),
+            handle: Cursor::new(Vec::new()),
         }
     }
 }
 
 // INFO: Record Based FileSystem: https://en.wikipedia.org/wiki/Record-oriented_filesystem
 impl<T: Seek + Read> Archive<T> {
-    pub fn from(storage: T) -> anyhow::Result<Archive<T>> {
-        Archive::with_config(storage, &HeaderConfig::default())
+    pub fn from(handle: T) -> anyhow::Result<Archive<T>> {
+        Archive::with_config(handle, &HeaderConfig::default())
     }
-    pub fn with_config(mut storage: T, config: &HeaderConfig) -> anyhow::Result<Archive<T>> {
-        match Archive::validate(&mut storage, config) {
+    
+    pub fn with_config(mut handle: T, config: &HeaderConfig) -> anyhow::Result<Archive<T>> {
+        match Archive::validate(&mut handle, config) {
             Ok(_) => {
-                let header = Header::from(&mut storage)?;
-                let registry = Registry::from(&mut storage, &header)?;
-                Result::Ok(Archive { header, registry, storage })
+                let header = Header::from(&mut handle)?;
+                let registry = Registry::from(&mut handle, &header)?;
+
+                Ok(Archive { header, registry, handle })
             }
-            Err(error) => Result::Err(error),
+            Err(error) => Err(error),
         }
     }
 
-    pub fn validate(storage: &mut T, config: &HeaderConfig) -> anyhow::Result<bool> {
-        let mut reader = BufReader::new(storage);
-        reader.seek(SeekFrom::Start(0))?;
+    pub fn fetch(&mut self, path: &str) -> anyhow::Result<Resource> {
+        self.registry.fetch(path, &mut self.handle)
+    }
 
+    pub fn validate(handle: &mut T, config: &HeaderConfig) -> anyhow::Result<bool> {
+        handle.seek(SeekFrom::Start(0))?;
+
+        // Validate magic
         let mut buffer = [0; HeaderConfig::MAGIC_LENGTH];
+        handle.read_exact(&mut buffer)?;
 
-        reader.read(&mut buffer)?;
-
-        if &buffer != &config.magic {
-            return Result::Err( anyhow::Error::msg(format!("Invalid magic found in archive: {}", str::from_utf8(&buffer)?)) );
+        if buffer != config.magic {
+            anyhow::bail!(format!("Invalid magic found in archive: {}", str::from_utf8(&buffer)?));
         };
 
+        // Validate version
         let mut buffer = [0; HeaderConfig::VERSION_SIZE];
-        reader.read(&mut buffer)?;
+        handle.read_exact(&mut buffer)?;
 
-        let archive_version = u16::from_ne_bytes(buffer);
+        let archive_version = u16::from_le_bytes(buffer);
         if config.minimum_version > archive_version {
-            return Result::Err(anyhow::Error::msg(format!(
+            anyhow::bail!(format!(
                 "Minimum Version requirement not met. Version found: {}, Minimum version: {}",
                 archive_version, config.minimum_version
-            )));
+            ))
         };
 
-        Result::Ok(true)
-    }
-
-    // Filesystem functions
-    pub fn fetch(&mut self, path: &String) -> anyhow::Result<&Resource> {
-        let mut reader = BufReader::new(&mut self.storage);
-        self.registry.fetch(path, &mut reader)
-    }
-}
-
-impl<T: Read + Seek + Write> Archive<T> {
-    pub fn append(&mut self, resource: &Resource, path: &String) -> anyhow::Result<RegistryEntry> {
-        let mut writer = BufWriter::new(&mut self.storage);
-        self.registry.append(path, resource, &mut writer)
-    }
-    pub fn delete(&mut self, path: &String) -> anyhow::Result<()> {
-        let mut writer = BufWriter::new(&mut self.storage);
-        self.registry.delete(path, &mut writer)
+        Ok(true)
     }
 }
 
 impl<T> fmt::Display for Archive<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        unimplemented!();
+        write!(f, "{header}\n{registry}", header=self.header, registry=self.registry)
     }
 }
