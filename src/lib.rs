9def2015559171bfe5dd52c1f381fcb714e0a903
sokorototo <nyachiengatoto@gmail.com>
2021-08-24 18:59:59 +0300

    Externalized tests, added writer export

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,23 @@
 #![allow(unused)]
+mod tests;
 
-pub(crate) mod global;
-pub(crate) mod loader;
+pub mod global;
+pub mod loader;
+pub mod writer;
+
+// Current archive version
+pub const VERSION: f32 = 0.1;
 
 // Simplify imports
-pub(crate) mod prelude {
+pub mod prelude {
     pub use crate::global::{
         header::{Header, HeaderConfig},
         registry::{Registry, RegistryEntry},
         types::*,
     };
     pub use crate::loader::{archive::Archive, resource::Resource};
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{global::{flags::RegEntryFlags, header::{Header, HeaderConfig}, registry::{Registry, RegistryEntry}}, loader::archive::Archive};
-    use std::{
-        fs::File,
-        io::{Read, Seek, Write},
+    pub use crate::writer::{
+        builder::{Builder, BuilderConfig},
+        leaf::{Leaf, LeafConfig}
     };
-    use ed25519_dalek as esdalek;
-
-    #[test]
-    fn defaults() {
-        let _header_config = HeaderConfig::new();
-        let _header = Header::empty();
-        let _registry = Registry::empty();
-        let _registry_entry = RegistryEntry::empty();
-    }
-
-    #[test]
-    fn header_config() -> anyhow::Result<()> {
-        let config = HeaderConfig::from(*b"VfACH", 0u16, None);
-        let mut file = File::open("me.vach")?;
-        format!("{}", &config);
-
-        let mut _header = Header::from(&mut file)?;
-        format!("{}", _header);
-
-        Archive::validate(&mut file, &config)?;
-
-        Ok(())
-    }
-
-    #[test]
-    pub fn esdalek_test() -> anyhow::Result<()>{
-        println!("Bytes per esdalek::Signature: {}", esdalek::SIGNATURE_LENGTH);
-        Ok(())
-    }
 }
