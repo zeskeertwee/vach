14e68aa9a9eca838132f84d563fa12c49a250ead
sokorototo <nyachiengatoto@gmail.com>
2021-08-25 16:26:27 +0300

    Generic handles instead of vectors

--- src/writer/leaf/mod.rs
+++ src/writer/leaf/mod.rs
@@ -7,45 +7,31 @@ use crate::{
 		types::FlagType
 	}
 };
-use std::{io::Read, fs::File};
+use std::{io::{Cursor, Read}};
 
-pub struct Leaf {
-    pub data: Vec<u8>,
-    content_version: u8,
-    flags: FlagType,
+#[derive(Debug)]
+pub struct Leaf<T> {
+    pub handle: T,
+    pub config: LeafConfig
 }
 
-impl Leaf {
-    pub fn empty() -> Leaf {
+impl Leaf<Cursor<Vec<u8>>> {
+    pub fn empty() -> Leaf<Cursor<Vec<u8>>> {
         Leaf {
-            data: vec![],
-            content_version: 0,
-            flags: 0,
+            handle: Cursor::new(vec![]),
+            config: LeafConfig::default()
         }
     }
-    pub fn from<T: Read>(data: &mut T, config: &LeafConfig) -> anyhow::Result<Leaf> {
-        let mut buffer = vec![];
-        data.read_to_end(&mut buffer)?;
-        Ok(Leaf {
-            data: buffer,
-            content_version: config.version,
-            flags: config.flags,
-        })
-    }
-    pub fn from_file(path: &str, config: &LeafConfig) -> anyhow::Result<Leaf> {
-        let mut file = File::open(path)?;
-        let mut vector = vec![];
-        file.read_to_end(&mut vector);
-        Ok(Leaf {
-            data: vector,
-            content_version: config.version,
-            flags: config.flags,
-        })
+}
+
+impl<T: Read> Leaf<T> {
+    pub fn from(handle: T, config: LeafConfig) -> anyhow::Result<Leaf<T>> {
+        Ok(Leaf { handle, config })
     }
     pub fn to_registry_entry(&self) -> RegistryEntry {
         RegistryEntry {
-            flags: self.flags,
-            content_version: self.content_version,
+            flags: FlagType::empty(),
+            content_version: self.config.content_version,
             signature: None,
 
             length: 0,
