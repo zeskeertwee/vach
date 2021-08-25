c71e642887a0dda097bc436982e9e2085069746a
sokorototo <nyachiengatoto@gmail.com>
2021-08-25 16:26:39 +0300

    Some utility functions

--- src/writer/leaf/config.rs
+++ src/writer/leaf/config.rs
@@ -1,7 +1,31 @@
-use crate::global::types::FlagType;
+use crate::{global::types::FlagType};
 
+#[derive(Clone, Debug)]
 pub struct LeafConfig {
-	pub flags: FlagType,
-	pub path: String,
-	pub version: u8,
+	pub id: String,
+	pub content_version: u8,
+	pub compress: bool,
+	pub sign: bool
+}
+
+impl LeafConfig {
+	 pub fn default() -> LeafConfig {
+		 LeafConfig { id: String::new(), content_version: 0, compress: true, sign: false }
+	 }
+	 pub fn compress(mut self, compress: bool) -> Self {
+		 self.compress = compress;
+		 self
+	 }
+	 pub fn sign(mut self, sign: bool) -> Self {
+		self.sign = sign;
+		self
+	}
+	pub fn version(mut self, version: u8) -> Self {
+		self.content_version = version;
+		self
+	}
+	pub fn id<S: ToString>(mut self, id: &S) -> Self {
+		self.id = id.to_string();
+		self
+	}
 }
