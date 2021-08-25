0b08d1b9d9f684782c9948ab3c1fb317fe3f7d27
sokorototo <nyachiengatoto@gmail.com>
2021-08-25 16:31:12 +0300

    Too much to document here

--- src/writer/builder/mod.rs
+++ src/writer/builder/mod.rs
@@ -1,104 +1,149 @@
 mod config;
-pub use config::BuilderConfig;
 use super::leaf::{Leaf, LeafConfig};
-use crate::global::{header::HeaderConfig, registry::RegistryEntry, types::RegisterType};
+use crate::global::{
+    header::HeaderConfig,
+    registry::RegistryEntry,
+    types::{FlagType, RegisterType},
+};
+pub use config::BuilderConfig;
 
-use std::{collections::HashMap, io::{Read, Write}};
-use ed25519_dalek::Signer;
+use ed25519_dalek::{self as esdalek, Signer};
+use hashbrown::HashMap;
 use lz4_flex as lz4;
+use std::io::{Cursor, Read, Write};
 
-pub struct Builder {
-    leafs: HashMap<String, Leaf>,
+#[derive(Debug)]
+pub struct Builder<T> {
+    leafs: HashMap<String, Leaf<T>>,
 }
 
-impl Builder {
-    pub fn empty() -> Self {
-        Self {
+impl<T: Read> Default for Builder<T> {
+    fn default() -> Builder<T> {
+        Builder {
             leafs: HashMap::new(),
         }
     }
+}
 
-    pub fn add<S: ToString, T: Read>(
-        &mut self,
-        mut data: T,
-        config: &LeafConfig,
-    ) -> anyhow::Result<()> {
-        let leaf = Leaf::from(&mut data, config)?;
-        self.leafs.insert(config.path.clone(), leaf);
+impl<T: Read> Builder<T> {
+    pub fn add(&mut self, data: T, path: &str) -> anyhow::Result<()> {
+        let mut config = LeafConfig::default();
+        config.id = path.to_string();
+        let leaf = Leaf::from(data, config.clone())?;
+        self.leafs
+            .insert(config.id.clone(), leaf)
+            .ok_or(anyhow::anyhow!("Unable to append data"))?;
+        Ok(())
+    }
+    pub fn add_with_config(&mut self, data: T, leaf_config: &LeafConfig) -> anyhow::Result<()> {
+        let leaf = Leaf::from(data, leaf_config.clone())?;
+        self.leafs
+            .insert(leaf_config.id.clone(), leaf)
+            .ok_or(anyhow::anyhow!("Unable to append data"))?;
+        Ok(())
+    }
+    pub fn add_leaf(&mut self, leaf: Leaf<T>) -> anyhow::Result<()> {
+        self.leafs
+            .insert(leaf.config.id.clone(), leaf)
+            .ok_or(anyhow::anyhow!("Unable to add leaf"))?;
         Ok(())
     }
 
-    pub fn write<W: Write>(&self, target: &mut W, config: &BuilderConfig) -> anyhow::Result<()> {
+    pub fn write<W: Write>( &mut self, target: &mut W, config: &BuilderConfig ) -> anyhow::Result<()> {
         // Write header in order defined in the spec document
-        target.write_all(&config.header.magic)?;
-        target.write_all(&config.flags.to_le_bytes())?;
-        target.write_all(&config.header.minimum_version.to_le_bytes())?;
+        target.write_all(&config.header_config.magic)?;
+
+        // Some interior mutability sorcery
+        let mut flags_temp = config.flags;
+        if config.keypair.is_some() { flags_temp.insert(FlagType::SIGNED) };
+        target.write_all(&flags_temp.bits().to_le_bytes())?;
+
+        target.write_all(&config.header_config.minimum_version.to_le_bytes())?;
         target.write_all(&(self.leafs.len() as u16).to_le_bytes())?;
 
         // Write the registry
         let mut leaf_data = vec![];
-        {
-            let mut registry_buffer = vec![];
-            let mut leaf_offset = HeaderConfig::BASE_SIZE;
-            //   let mut data:Vec<(String, RegistryEntry, Vec<u8>)> = vec![];
+        let mut registry_buffer = vec![];
+        
+        // Calculate the size of the registry
+        let mut reg_size = 0usize;
+        for (path, _) in self.leafs.iter() {
+            reg_size += (path.len() + RegistryEntry::MIN_SIZE)
+        }
+        
+        // Start counting the offset of the leafs from the end of the registry
+        let mut leaf_offset = reg_size + HeaderConfig::BASE_SIZE;
 
-            // Calculate the size of the registry
-            let mut reg_size = 0usize;
-            for (path, _) in self.leafs.iter() {
-                reg_size += (path.len() + RegistryEntry::MIN_SIZE)
-            }
+        // Populate the archive glob
+        for (path, leaf) in self.leafs.iter_mut() {
+            let mut entry = leaf.to_registry_entry();
+            let mut glob = vec![];
+            let mut compressed_data;
 
-            // Start counting the offset of the leafs from the end of the registry
-            leaf_offset += reg_size;
+            leaf.handle.read_to_end(&mut glob);
+            let length = glob.len() as u64;
 
-            // Populate the archive glob
-            for (path, leaf) in self.leafs.iter() {
-                let mut entry = leaf.to_registry_entry();
-                let mut glob;
-
-                // Create and compare compressed leaf data
-                let compressed_data = lz4::compress_prepend_size(&leaf.data);
-                let ratio: f64 = compressed_data.len() as f64 / leaf.data.len() as f64;
-
-                if compressed_data.len() >= leaf.data.len() {
-                    println!("Did not compress {} ({:.2}x original size)", path, ratio);
-                    entry.signature = Some(config.keypair.sign(&leaf.data));
-                    glob = leaf.data.clone()
+            // Create and compare compressed leaf data
+            if leaf.config.compress {
+                compressed_data = lz4::compress_prepend_size(&glob);
+                let ratio = (compressed_data.len() as f32 / length as f32);
+                if ratio < 1f32 {
+                    println!("Compressed {} ({:2}x original size)", path, ratio);
+                    glob = compressed_data;
+                    entry.flags.insert(FlagType::COMPRESSED);
                 } else {
-                    println!("Compressed {} ({:.2}x original size)", path, ratio);
-                    entry.signature = Some(config.keypair.sign(&compressed_data));
-                    glob = compressed_data
+                    drop(compressed_data);
                 };
-
-                let glob_length = glob.len();
-                leaf_data.extend(glob);
-
-                leaf_offset += glob_length;
-                entry.length = glob_length as RegisterType;
-                entry.location = leaf_offset as RegisterType;
-
-                let mut entry_b = entry.bytes(&(path.len() as u64));
-                entry_b.extend(path.as_bytes());
-                registry_buffer.extend(entry_b);
-            }
-
-            // Assert that the reg_size did indeed match the size of the registry buffer
-            assert_eq!(reg_size, registry_buffer.len());
-
-            {
-                // As we are still writing the Header to the target, write_all the signature bytes
-                // In it's own scope so that the below values are dropped prematurely, so we release some memory
-                let signature = config.keypair.sign(&registry_buffer);
-                let signature_bytes = signature.to_bytes();
-                target.write_all(&signature_bytes)?;
             };
 
-            // Write the registry out
-            target.write_all(&registry_buffer)?;
-        };
+            let glob_length = glob.len();
+            leaf_data.extend(&glob);
+
+            entry.location = leaf_offset as RegisterType;
+            leaf_offset += glob_length;
+            entry.length = glob_length as RegisterType;
+
+            if leaf.config.sign {
+                entry.signature = Some(
+                    config
+                        .keypair
+                        .as_ref()
+                        .ok_or(anyhow::anyhow!(
+                            "No keypair provided, yet leaf-config requires the leaf be signed"
+                        ))?
+                        .sign(&glob),
+                );
+                entry.flags.insert(FlagType::SIGNED);
+            };
+
+            // Drop stuff
+            drop(glob);
+
+            let mut entry_b = entry.bytes(&(path.len() as u64));
+            entry_b.extend(path.as_bytes());
+            registry_buffer.extend(entry_b);
+        }
+
+        {
+            // Sign the registry and write out the signature
+            match &config.keypair {
+                Some(keypair) => {
+                    let signature = keypair.sign(&registry_buffer);
+                    let signature_bytes = signature.to_bytes();
+                    target.write_all(&signature_bytes)?;
+                },
+                None => {
+                    target.write_all(&[0x53u8; esdalek::SIGNATURE_LENGTH])?;
+                }
+            }
+        }
+        // Write out the registry
+        target.write_all(&registry_buffer)?;
+        drop(registry_buffer);
 
         // Write the glob
         target.write_all(&leaf_data)?;
+        drop(leaf_data);
 
         Ok(())
     }
