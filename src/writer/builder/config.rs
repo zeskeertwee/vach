299dba34c486820a31c6fd8870e0a50439ff2f77
sokorototo <nyachiengatoto@gmail.com>
2021-08-25 16:32:21 +0300

    impl Default

--- src/writer/builder/config.rs
+++ src/writer/builder/config.rs
@@ -1,8 +1,15 @@
 use crate::global::{types::FlagType, header::HeaderConfig};
 use ed25519_dalek as esdalek;
 
+#[derive(Debug)]
 pub struct BuilderConfig {
-    pub header: HeaderConfig,
+    pub header_config: HeaderConfig,
     pub flags: FlagType,
-    pub keypair: esdalek::Keypair,
+    pub keypair: Option<esdalek::Keypair>,
+}
+
+impl Default for BuilderConfig {
+    fn default() -> BuilderConfig {
+        BuilderConfig { header_config: HeaderConfig::default(), flags: FlagType::default(), keypair: None }
+    }
 }
