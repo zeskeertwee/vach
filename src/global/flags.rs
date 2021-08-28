30c59b37661bc2656ae95029132b5eef43c42abe
sokorototo <nyachiengatoto@gmail.com>
2021-08-28 19:22:50 +0300

    Test data

--- src/global/flags.rs
+++
@@ -1,9 +0,0 @@
-use bitflags::bitflags;
-
-bitflags! {
-	#[derive(Default)]
-    pub struct Flags: u16 {
-            const SIGNED = 0b_0100_0000_0000_0000;
-            const COMPRESSED = 0b_1000_0000_0000_0000;
-    }
-}
