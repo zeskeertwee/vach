6d64600ba90c0c11583eccf558414c51dd449479
sokorototo <nyachiengatoto@gmail.com>
2021-08-29 21:38:08 +0300

    Visibility changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,30 @@
 #![allow(clippy::or_fun_call)]
 mod tests;
 
-pub mod global;
-pub mod loader;
-pub mod writer;
+pub(crate) mod global;
+pub(crate) mod loader;
+pub(crate) mod writer;
 
 // Re-exports
 pub use rand;
 pub use anyhow;
-pub use bitflags;
 
 // Global constants
 pub const VERSION: u16 = 12;
 pub const KEYPAIR_LENGTH: usize = ed25519_dalek::KEYPAIR_LENGTH;
+pub const PUBLIC_KEY_LENGTH: usize = ed25519_dalek::PUBLIC_KEY_LENGTH;
+pub const SIGNATURE_LENGTH: usize =  ed25519_dalek::SIGNATURE_LENGTH;
 
 // Simpler imports
 pub mod prelude {
     pub use crate::global::{
         header::{Header, HeaderConfig},
-        registry::{Registry, RegistryEntry},
         types::*,
     };
-    pub use crate::loader::{archive::Archive, resource::Resource};
+    pub use crate::loader::{
+        archive::Archive,
+        resource::Resource
+    };
     pub use crate::writer::{
         builder::{Builder, BuilderConfig},
         leaf::{Leaf, LeafConfig}
