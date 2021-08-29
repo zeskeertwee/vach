bce11af52b536873fde35aab66c937eedf256b4e
sokorototo <nyachiengatoto@gmail.com>
2021-08-29 23:27:16 +0300

    _

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub use anyhow;
 // Global constants
 pub const VERSION: u16 = 12;
 pub const KEYPAIR_LENGTH: usize = ed25519_dalek::KEYPAIR_LENGTH;
+pub const SECRET_KEY_LENGTH: usize = ed25519_dalek::SECRET_KEY_LENGTH;
 pub const PUBLIC_KEY_LENGTH: usize = ed25519_dalek::PUBLIC_KEY_LENGTH;
 pub const SIGNATURE_LENGTH: usize =  ed25519_dalek::SIGNATURE_LENGTH;
 
