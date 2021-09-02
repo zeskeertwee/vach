db4808a07d0a180f5a8701e4432ac2279d176811
Jasper Fortuin <zeskeertwee@gmail.com>
2021-09-02 21:27:26 +0200

    refactor writer for changed spec

--- virtfs-ach/src/writer/mod.rs
+++ virtfs-ach/src/writer/mod.rs
@@ -59,52 +59,51 @@ impl ArchiveBuilder {
         Ok(())
     }
 
-    pub fn bytes<S: Signer<ed25519_dalek::Signature>>(&self, signer: &S) -> Vec<u8> {
+    pub fn bytes<S: Signer<ed25519_dalek::Signature>>(&mut self, signer: &S) -> Vec<u8> {
         let mut buffer = Vec::new();
         
         buffer.extend_from_slice(MAGIC);
         buffer.extend_from_slice(&CURRENT_ARCHIVE_VERSION.to_le_bytes());
         buffer.extend_from_slice(&(self.leafs.len() as u16).to_le_bytes());
 
-        let mut registry_buffer: Vec<u8> = Vec::new();
-        let mut registry_entries = Vec::new();
-        let mut offset: usize = Header::SIZE;
         let mut processed_data = Vec::new();
 
-        for leaf in self.leafs.iter() {
+        for leaf in self.leafs.iter_mut() {
             let mut registry_entry = leaf.to_registry_entry();
             
-            let compressed_data = lz4_flex::compress_prepend_size(&leaf.data);
+            let mut compressed_data = lz4_flex::compress_prepend_size(&leaf.data);
             let size_diff: f64 = compressed_data.len() as f64 / leaf.data.len() as f64;
             if compressed_data.len() >= leaf.data.len() {
                 info!("Did not compress {} ({:.2}x original size)", registry_entry.path_string().unwrap(), size_diff);
+
+                let original_leaf_data_size = leaf.data.len();
+                leaf.data.extend(&leaf.path);
+
                 let signature = signer.sign(&leaf.data);
+
+                leaf.data.truncate(original_leaf_data_size);
+
                 registry_entry.blob_signature = signature.to_bytes();
                 registry_entry.compressed_size = leaf.data.len() as u32;
                 processed_data.push(leaf.data.clone());
             } else {
                 info!("Compressed {} ({:.2}x original size)", registry_entry.path_string().unwrap(), size_diff);
-                let signature = signer.sign(&compressed_data);
                 registry_entry.flags.set(RegistryEntryFlags::IS_COMPRESSED, true);
+
+                let original_compressed_size = compressed_data.len();
+                compressed_data.extend(&leaf.path);
+
+                let signature = signer.sign(&compressed_data);
+
+                compressed_data.truncate(original_compressed_size);
+
                 registry_entry.blob_signature = signature.to_bytes();
                 registry_entry.compressed_size = compressed_data.len() as u32;
                 processed_data.push(compressed_data);
             }
 
-            offset += registry_entry.size();
-            registry_entries.push(registry_entry);
+            buffer.extend(registry_entry.bytes());
         }
-
-        for (entry, data) in registry_entries.iter_mut().zip(&processed_data) {
-            entry.byte_offset = offset as u64;
-            offset += data.len();
-            registry_buffer.extend_from_slice(&entry.bytes());
-        }
-
-        let signature = signer.sign(&registry_buffer);
-        let signature_bytes = signature.as_bytes();
-        buffer.extend_from_slice(signature_bytes);
-        buffer.extend_from_slice(&registry_buffer);
 
         for data in processed_data {
             buffer.extend_from_slice(&data);
