cde41f858f91a03b1c03bc9058d4ac1e9b4016eb
sokorototo <nyachiengatoto@gmail.com>
2021-09-17 21:34:37 +0300

    Some serious refactoring

--- vach/src/global/types.rs
+++ vach/src/global/types.rs
@@ -1,86 +1,63 @@
-use anyhow::{Result, bail};
+use anyhow;
 
-pub const RESERVED_MASK: u16 = 0b1111_0000_0000_0000;
-pub const COMPRESSED_FLAG: u16 = 0b_1000_0000_0000_0000;
-pub const SIGNED_FLAG: u16 = 0b_0100_0000_0000_0000;
+// Private utility function
+fn _contains(first: u16, other: u16) -> bool {
+	!((first & other) == 0)
+}
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct Flags {
-    bits: u16,
+	bits: u16,
 }
 
 // based on code in https://github.com/bitflags/bitflags/blob/main/src/lib.rs
-// basically just a tiny version of bitflags
+// basically just a tiny, scoped version of bitflags
 impl Flags {
-    pub(crate) fn from_bits(bits: u16) -> Self {
-        Self {
-            bits,
-        }
-    }
+	// Scoped constants
+	pub const RESERVED_MASK: u16 = 0b1111_0000_0000_0000;
+	pub const COMPRESSED_FLAG: u16 = 0b_1000_0000_0000_0000;
+	pub const SIGNED_FLAG: u16 = 0b_0100_0000_0000_0000;
 
-    pub fn bits(&self) -> u16 {
-        self.bits
-    }
+	#[inline(always)]
+	pub fn from_bits(bits: u16) -> Self {
+		Self { bits }
+	}
+	#[inline(always)]
+	pub fn bits(&self) -> u16 {
+		self.bits
+	}
+	#[inline(always)]
+	pub fn empty() -> Self {
+		Self { bits: 0 }
+	}
 
-    pub(crate) fn _set(&mut self, mask: u16, value: bool) {
-        if value {
-            self.bits |= mask;
-        } else {
-            self.bits &= !mask;
-        }
-    }
+	/// Returns a error if mask contains a reserved bit
+	pub fn set(&mut self, mask: u16, toggle: bool) -> anyhow::Result<u16> {
+		if _contains(Flags::RESERVED_MASK, mask) {
+			anyhow::bail!("Tried to set reserved bit(s)!");
+		} else {
+			self.force_set(mask, toggle)
+		}
 
-    /// returns a error if mask contains a reserved bit
-    pub fn set(&mut self, mask: u16, value: bool) -> anyhow::Result<()> {
-        if _contains(RESERVED_MASK, mask) {
-            bail!("Tried to set reserved bit(s)!");
-        } else {
-            self._set(mask, value);
-        }
+		Ok(self.bits)
+	}
+	pub(crate) fn force_set(&mut self, mask: u16, toggle: bool) {
+		if toggle {
+			 self.bits |= mask;
+		} else {
+			 self.bits &= !mask;
+		}
+  }
 
-        Ok(())
-    }
-
-    pub fn contains(&self, mask: u16) -> bool {
-        _contains(self.bits, mask)
-    }
-}
-
-fn _contains(first: u16, other: u16) -> bool {
-    !((first & other) == 0)
+	#[inline(always)]
+	pub fn contains(&self, mask: u16) -> bool {
+		_contains(self.bits, mask)
+	}
 }
 
 impl Default for Flags {
-    fn default() -> Self {
-        Self {
-            bits: 0,
-        }
-    }
-}
-
-#[test]
-fn bitflags_reserved_protection() {
-    let initial_flags = 0b1111_1000_0000_0000;
-    let custom_flag = 0b0000_0000_0000_0100;
-    let mut flag = Flags { bits: initial_flags };
-    assert!(flag.set(COMPRESSED_FLAG, true).is_err());
-    assert!(flag.contains(initial_flags));
-    assert!(flag.set(custom_flag, true).is_ok());
-}
-
-#[test]
-fn bitflags_set_intersects() {
-    let mut flag = Flags::default();
-    flag._set(COMPRESSED_FLAG, true);
-    assert_eq!(flag.bits, COMPRESSED_FLAG);
-    flag._set(COMPRESSED_FLAG, true);
-    assert_eq!(flag.bits, COMPRESSED_FLAG);
-    flag._set(SIGNED_FLAG, true);
-    assert_eq!(flag.bits, COMPRESSED_FLAG | SIGNED_FLAG);
-    flag._set(COMPRESSED_FLAG, false);
-    assert_eq!(flag.bits, SIGNED_FLAG);
-    flag._set(COMPRESSED_FLAG, false);
-    assert_eq!(flag.bits, SIGNED_FLAG);
-    flag._set(COMPRESSED_FLAG | SIGNED_FLAG, true);
-    assert_eq!(flag.bits, COMPRESSED_FLAG | SIGNED_FLAG);
+	#[inline(always)]
+	fn default() -> Self {
+		Self { bits: 0 }
+	}
 }
