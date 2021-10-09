fcf7f90a428fa99533af082a2435abc540027fb9
sokorototo <nyachiengatoto@gmail.com>
2021-10-09 22:00:34 +0300

    _

--- vach/src/lib.rs
+++ vach/src/lib.rs
@@ -15,7 +15,7 @@ pub(crate) mod writer;
 pub use rand;
 
 /// Current file spec version, both `Loader` and `Builder`
-pub const VERSION: u16 = 16;
+pub const VERSION: u16 = 17;
 
 /// Size of a keypair: (secret + public)
 pub const KEYPAIR_LENGTH: usize = 64;
