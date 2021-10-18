3fb796393fb619cdba0d2b8c387cfec63c2c21ed
sokorototo <nyachiengatoto@gmail.com>
2021-10-18 20:40:52 +0300

    pub(crate) is useless if struct is pub(crate)

--- vach/src/global/header.rs
+++ vach/src/global/header.rs
@@ -12,12 +12,12 @@ use ed25519_dalek as esdalek;
 
 /// Used to configure and give extra information to the `Archive` loader.
 /// Used exclusively in archive source and integrity validation.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct HeaderConfig {
 	/// If the archive has a custom magic sequence, pass the custom _MAGIC_ sequence here.
 	/// The custom _MAGIC_ sequence can then be used to validate archive sources.
 	pub magic: [u8; crate::MAGIC_LENGTH],
-	/// An ed25519 public key. **If no key is provided, is `None`, then signature validation is ignored**. Even if the
+	/// An ed25519 public key. **If no key is provided, (is `None`), then signature validation is ignored**. Even if the
 	/// archive source has signatures.
 	pub public_key: Option<esdalek::PublicKey>,
 }
@@ -28,6 +28,7 @@ impl HeaderConfig {
 	/// use vach::prelude::HeaderConfig;
 	/// let config = HeaderConfig::new(*b"_TEST",  None);
 	/// ```
+	#[inline(always)]
 	pub fn new(magic: [u8; 5], key: Option<esdalek::PublicKey>) -> HeaderConfig {
 		HeaderConfig {
 			magic,
@@ -36,10 +37,13 @@ impl HeaderConfig {
 	}
 
 	/// Shorthand to load and parse an ed25519 public key from a `Read` handle, into this `HeaderConfig`,
-	/// ```ignore
-	/// let keypair_file = File::open(KEYPAIR)?;
-	/// config.load_public_key(keypair_file)?;
 	/// ```
+	/// use vach::{utils::gen_keypair, prelude::HeaderConfig};
+	/// let mut config = HeaderConfig::default();
+	/// let keypair_bytes = gen_keypair().to_bytes();
+	/// config.load_public_key(&keypair_bytes[32..]).unwrap();
+	/// ```
+	#[inline]
 	pub fn load_public_key<T: Read>(&mut self, mut handle: T) -> anyhow::Result<()> {
 		let mut keypair_bytes = [4; crate::PUBLIC_KEY_LENGTH];
 		handle.read_exact(&mut keypair_bytes)?;
@@ -67,8 +71,8 @@ impl fmt::Display for HeaderConfig {
 			f,
 			"[HeaderConfig] magic: {}, has_public_key: {}",
 			match str::from_utf8(&self.magic) {
-				Ok(_str) => {
-					_str
+				Ok(magic) => {
+					magic
 				}
 				Err(_) => {
 					return fmt::Result::Err(fmt::Error);
@@ -108,15 +112,15 @@ impl Default for Header {
 
 impl Header {
 	// BASE_SIZE => 11 + 64 = 75
-	pub(crate) const BASE_SIZE: usize =
+	pub const BASE_SIZE: usize =
 		crate::MAGIC_LENGTH + Self::FLAG_SIZE + Self::VERSION_SIZE + Self::CAPACITY_SIZE;
 
 	// Data appears in this order
-	pub(crate) const FLAG_SIZE: usize = 2;
-	pub(crate) const VERSION_SIZE: usize = 2;
-	pub(crate) const CAPACITY_SIZE: usize = 2;
+	pub const FLAG_SIZE: usize = 2;
+	pub const VERSION_SIZE: usize = 2;
+	pub const CAPACITY_SIZE: usize = 2;
 
-	pub(crate) fn validate(header: &Header, config: &HeaderConfig) -> anyhow::Result<()> {
+	pub fn validate(header: &Header, config: &HeaderConfig) -> anyhow::Result<()> {
 		// Validate magic
 		if header.magic != config.magic {
 			anyhow::bail!(format!(
@@ -128,7 +132,7 @@ impl Header {
 		// Validate version
 		if crate::VERSION > header.arch_version {
 			anyhow::bail!(format!(
-                "The provided archive source has version: {}. While the loader has a version: {}. The current loader is out of date!",
+                "The provided archive source has version: {}. While the loader has a version: {}. The current loader is likely out of date!",
                 header.arch_version, crate::VERSION
             ))
 		};
@@ -136,7 +140,7 @@ impl Header {
 		Ok(())
 	}
 
-	pub(crate) fn from_handle<T: Read + Seek>(mut handle: T) -> anyhow::Result<Header> {
+	pub fn from_handle<T: Read + Seek>(mut handle: T) -> anyhow::Result<Header> {
 		handle.seek(SeekFrom::Start(0))?;
 		let mut buffer = [0x69; Header::BASE_SIZE];
 		handle.read_exact(&mut buffer)?;
