0adceec30e1d79bfe7533133dc3d534330bf9f42
sokorototo <nyachiengatoto@gmail.com>
2021-10-22 12:05:14 +0300

    read_keypair is public

--- vach/src/utils/mod.rs
+++ vach/src/utils/mod.rs
@@ -10,8 +10,8 @@ pub fn gen_keypair() -> esdalek::Keypair {
 	esdalek::Keypair::generate(&mut OsRng)
 }
 
-// Use this to read and parse a `Keypair` from a `io::Read` handle
-pub(crate) fn read_keypair<R: Read>(mut handle: R) -> anyhow::Result<esdalek::Keypair> {
+/// Use this to read and parse a `Keypair` from a `io::Read` handle
+pub fn read_keypair<R: Read>(mut handle: R) -> anyhow::Result<esdalek::Keypair> {
 	let mut keypair_bytes = [0; crate::KEYPAIR_LENGTH];
 	handle.read_exact(&mut keypair_bytes)?;
 	Ok(esdalek::Keypair::from_bytes(&keypair_bytes)?)
