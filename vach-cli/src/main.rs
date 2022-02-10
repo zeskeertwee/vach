4d622f2044e0e8a2e7150f0d108b2b6d132ca1ff
sokorototo <nyachiengatoto@gmail.com>
2022-02-10 13:40:12 +0300

    It's a plain print,

--- vach-cli/src/main.rs
+++ vach-cli/src/main.rs
@@ -24,7 +24,7 @@ fn main() {
 	let res = match matches.subcommand() {
 		Some((key, mtx)) => commands.get(key).unwrap().evaluate(mtx),
 		None => {
-			log::info!("vach-cli: Run `vach --help` for usage");
+			println!("vach-cli: Run `vach --help` for usage");
 			Ok(())
 		},
 	};
